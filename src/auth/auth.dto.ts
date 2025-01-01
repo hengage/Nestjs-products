eec5fe4772e458d3a6b331c441c17fa321288023
hengage <tunecarterkillz@gmail.com>
2025-01-01 15:22:07 +0100

    feat(validation): validate auth endpoints with class-validators

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,10 +1,39 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
+
 export class CreateUserDto {
-  email: string;
-  password: string;
+  @MinLength(2, { message: 'Name must be at least 2 characters' })
+  @MaxLength(50, { message: 'Name cannot exceed 50 characters' })
+  @IsString({ message: 'Name must be a string' })
+  @IsNotEmpty({ message: 'Name is required' })
   name: string;
+
+  @MinLength(2, { message: 'Email must be at least 6 characters' })
+  @MaxLength(50, { message: 'Email cannot exceed 50 characters' })
+  @IsString({ message: 'Email must be a string' })
+  @IsEmail({}, { message: 'Email must be a valid email format' })
+  @IsNotEmpty({ message: 'Email is required' })
+  email: string;
+
+  @MinLength(6, { message: 'Password must be at least 6 characters' })
+  @MaxLength(100, { message: 'Password cannot exceed 100 characters' })
+  @IsString({ message: 'Password must be a string' })
+  @IsNotEmpty({ message: 'Password is required' })
+  password: string;
 }
 
 export class LoginDto {
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsString({ message: 'Email must be a string' })
+  @IsEmail({}, { message: 'Email must be a valid email format' })
   email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @IsString({ message: 'Password must be a string' })
   password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from './config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(CorsOptions);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      stopAtFirstError: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
