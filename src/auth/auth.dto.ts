b383b28cc4431852ac814d83682063a8d222342f
hengage <tunecarterkillz@gmail.com>
2025-01-01 16:43:57 +0100

    refactor(validation): DRY with validation message and rules

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -2,38 +2,52 @@ import {
   IsEmail,
   IsNotEmpty,
   IsString,
-  MinLength,
   MaxLength,
+  MinLength,
 } from 'class-validator';
+import { LENGTH_RULES, VALIDATION_FIELDS } from 'src/constants';
+import { Msg } from 'src/utils/message';
 
 export class CreateUserDto {
-  @MinLength(2, { message: 'Name must be at least 2 characters' })
-  @MaxLength(50, { message: 'Name cannot exceed 50 characters' })
-  @IsString({ message: 'Name must be a string' })
-  @IsNotEmpty({ message: 'Name is required' })
+  @MinLength(LENGTH_RULES[VALIDATION_FIELDS.NAME].min, {
+    message: Msg.ERROR_MIN_LENGTH(VALIDATION_FIELDS.NAME),
+  })
+  @MaxLength(LENGTH_RULES[VALIDATION_FIELDS.NAME].max, {
+    message: Msg.ERROR_MAX_LENGTH(VALIDATION_FIELDS.NAME),
+  })
+  @IsString({ message: Msg.ERROR_STRING_TYPE(VALIDATION_FIELDS.NAME) })
+  @IsNotEmpty({ message: Msg.ERROR_REQUIRED(VALIDATION_FIELDS.NAME) })
   name: string;
 
-  @MinLength(2, { message: 'Email must be at least 6 characters' })
-  @MaxLength(50, { message: 'Email cannot exceed 50 characters' })
-  @IsString({ message: 'Email must be a string' })
-  @IsEmail({}, { message: 'Email must be a valid email format' })
-  @IsNotEmpty({ message: 'Email is required' })
+  @MinLength(LENGTH_RULES[VALIDATION_FIELDS.EMAIL].min, {
+    message: Msg.ERROR_MIN_LENGTH(VALIDATION_FIELDS.EMAIL),
+  })
+  @MaxLength(LENGTH_RULES[VALIDATION_FIELDS.EMAIL].max, {
+    message: Msg.ERROR_MAX_LENGTH(VALIDATION_FIELDS.EMAIL),
+  })
+  @IsString({ message: Msg.ERROR_STRING_TYPE(VALIDATION_FIELDS.EMAIL) })
+  @IsEmail({}, { message: Msg.ERROR_EMAIL_FORMAT() })
+  @IsNotEmpty({ message: Msg.ERROR_REQUIRED(VALIDATION_FIELDS.EMAIL) })
   email: string;
 
-  @MinLength(6, { message: 'Password must be at least 6 characters' })
-  @MaxLength(100, { message: 'Password cannot exceed 100 characters' })
-  @IsString({ message: 'Password must be a string' })
-  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(LENGTH_RULES[VALIDATION_FIELDS.PASSWORD].min, {
+    message: Msg.ERROR_MIN_LENGTH(VALIDATION_FIELDS.PASSWORD),
+  })
+  @MaxLength(LENGTH_RULES[VALIDATION_FIELDS.PASSWORD].max, {
+    message: Msg.ERROR_MAX_LENGTH(VALIDATION_FIELDS.PASSWORD),
+  })
+  @IsString({ message: Msg.ERROR_STRING_TYPE(VALIDATION_FIELDS.PASSWORD) })
+  @IsNotEmpty({ message: Msg.ERROR_REQUIRED(VALIDATION_FIELDS.PASSWORD) })
   password: string;
 }
 
 export class LoginDto {
-  @IsNotEmpty({ message: 'Email is required' })
-  @IsString({ message: 'Email must be a string' })
-  @IsEmail({}, { message: 'Email must be a valid email format' })
+  @IsNotEmpty({ message: Msg.ERROR_REQUIRED(VALIDATION_FIELDS.EMAIL) })
+  @IsString({ message: Msg.ERROR_STRING_TYPE(VALIDATION_FIELDS.EMAIL) })
+  @IsEmail({}, { message: Msg.ERROR_EMAIL_FORMAT() })
   email: string;
 
-  @IsNotEmpty({ message: 'Password is required' })
-  @IsString({ message: 'Password must be a string' })
+  @IsNotEmpty({ message: Msg.ERROR_REQUIRED(VALIDATION_FIELDS.PASSWORD) })
+  @IsString({ message: Msg.ERROR_STRING_TYPE(VALIDATION_FIELDS.PASSWORD) })
   password: string;
 }
